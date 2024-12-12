a277afd51a994557c06e812179ad7f84c798397a
trilogymercer <80383798+trilogymercer@users.noreply.github.com>
2024-12-12 10:30:38 +0400

    Updated KB ID

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,31 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const KnowledgeBaseDataSource =
+  backend.data.resources.graphqlApi.addHttpDataSource(
+    "KnowledgeBaseDataSource",
+    `https://bedrock-runtime.${cdk.Stack.of(backend.data).region}.amazonaws.com`,
+    {
+      authorizationConfig: {
+        signingRegion: cdk.Stack.of(backend.data).region,
+        signingServiceName: "bedrock",
+      },
+    },
+  );
+
+KnowledgeBaseDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      `arn:aws:bedrock:${cdk.Stack.of(backend.data).region}:783413248379:knowledge-base/BTTXNNXJBS`
+    ],
+    actions: ["bedrock:Retrieve"],
+  }),
+);
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,32 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  
+  knowledgeBase: a
+    .query()
+    .arguments({ input: a.string() })
+    .handler(
+      a.handler.custom({
+        dataSource: "KnowledgeBaseDataSource",
+        entry: "./resolvers/kbResolver.js",
+      }),
+    )
+    .returns(a.string())
+    .authorization((allow) => allow.authenticated()),
+
+  chat: a.conversation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant',  
+    tools: [
+      a.ai.dataTool({
+        name: 'searchDocumentation',
+        description: 'Performs a similarity search over the documentation for ...',
+        query: a.ref('knowledgeBase'),
+      }),
+    ]
+  })
+  .authorization((allow) => allow.owner()),
+
 });
 
 export type Schema = ClientSchema<typeof schema>;
